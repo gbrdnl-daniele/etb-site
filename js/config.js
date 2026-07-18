0eb2a1d23df80f01bc0549965b0e4683d7e12fc1
gbrdnl-daniele <gbrdnl@gmail.com>
2026-07-18 19:24:46 +0200

    Ripristino parziale modifiche

--- js/config.js
+++ js/config.js
@@ -3,11 +3,6 @@
 window.ETB_CONFIG = {
   bookingEmail: "booking@etberostribute.it",
 
-  contacts: {
-    phone1: "333.1234567",
-    phone2: "334.1234567",
-  },
-
   backend: {
     mock: false,
     sendQuoteEndpoint: "/api/send-quote",
